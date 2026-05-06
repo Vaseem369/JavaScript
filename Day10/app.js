8b0da521b53376a1f9a7353b8968decf947ac0b9
Vaseem Naushad <vaseemnaushad369@gmail.com>
2026-05-06 15:33:25 +0530

    Arranged the files according to the days

---
+++ Day10/app.js
@@ -0,0 +1,10 @@
+const title = document.querySelector("#title");
+
+// Change text
+console.log(title.textContent = "Hello, Vaseem!");
+
+title.style.color="#a12b2b";
+
+title.style.fontFamily="Georgia, serif";
+
+document.body.classList.toggle("dark")
---
+++ Day9/task.js
@@ -0,0 +1,4 @@
+const item ="Laptop";
+const price=60000;
+const tax=0.18;
+console.log(`The + ${item} costs ${}
